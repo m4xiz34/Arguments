41bfc634e3ac7c87a77b948da2575eeef8ecac64
m4xiz34 <maxisacroissant@gmail.com>
2026-03-25 19:57:26 +1100

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,16 @@
+def factorial(x):
+    """ This is a recursive function to find the factorial of am integer. """
+    
+    if x==0 or x==1:
+        return 1
+    else:
+        return x*factorial(x-1)
+        
+print(factorial.__doc__)
+print("the factorial of 0 is:", factorial(0))
+print("the factorial of 1 is:", factorial(1))
+print("the factorial of 2 is:", factorial(2))
+print("the factorial of 3 is:", factorial(3))
+print("the factorial of 4 is:", factorial(4))
+print("the factorial of 5 is:", factorial(5))
+print("the factorial of 10 is:", factorial(10))
